bc58ce6b9137e736806e0361a9531d9468c89cf1
ZumeShougo <g0109725rena@gmail.com>
2023-02-08 15:51:39 +0900

    Add useState

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
 import ReactDOM from "react-dom";
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
-  const todos = [
+  const [todos, setTodos] = useState([
     { id: 1, title: '姉に誕生日プレゼントを買う' },
     { id: 2, title: '論文提出' },
     { id: 3, title: '市役所に住民票の写しをもらいに行く' }
-  ]
+  ])
+
+  const [todoTitle, setTodoTitle] = useState('')
 
   return (
     <>
